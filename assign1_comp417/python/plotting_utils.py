ad17b02a83d22b5450cda11c0a0b6cf3dafc2ab4
David Meger <dmeger@Davids-MacBook-Pro.local>
2019-09-20 17:01:23 -0400

    A1 initial commit.

---
+++ assign1_comp417/python/plotting_utils.py
@@ -0,0 +1,41 @@
+import numpy as np
+import cv2
+
+"""
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+import matplotlib.patches as patches
+
+def draw_plan(world, plan):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.imshow(world)
+    for state in plan:
+        points = state.car_as_triangle()
+        points = points + [points[0]]
+        
+        codes = [Path.MOVETO,
+                 Path.LINETO,
+                 Path.LINETO,
+                 Path.CLOSEPOLY]
+
+        path = Path(points, codes)
+        patch = patches.PathPatch(path, facecolor='orange', lw=0.0)
+        ax.add_patch(patch)
+        
+    plt.show()
+"""
+
+def draw_plan(world, plan, bgr=(255,0,0), thickness=1):
+    img = np.copy(world)
+    cv2.circle(img, (int(plan[0].x), int(plan[0].y)), 3, (0,255,0), 3)
+    for t in xrange(len(plan)-1):
+        pt0 = (int(plan[t].x), int(plan[t].y))
+        pt1 = (int(plan[t+1].x), int(plan[t+1].y))
+        
+        cv2.line(img, pt0, pt1, bgr, thickness)
+        
+    cv2.circle(img, (int(plan[-1].x), int(plan[-1].y)), 3, (0,0,255), 3)
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
